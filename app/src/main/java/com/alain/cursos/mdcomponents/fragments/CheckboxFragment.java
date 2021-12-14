cd8bb78df00cbf3c103d25e332a661dd08caa9a2
Nolo-98 <twisterfast@gmail.com>
2021-12-14 17:47:30 -0600

    Checkbox-Indeterminate

--- app/src/main/java/com/alain/cursos/mdcomponents/fragments/CheckboxFragment.java
+++ app/src/main/java/com/alain/cursos/mdcomponents/fragments/CheckboxFragment.java
@@ -7,11 +7,16 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 import com.alain.cursos.mdcomponents.R;
 import com.alain.cursos.mdcomponents.utils.Component;
 import com.alain.cursos.mdcomponents.utils.Constants;
+import com.buildware.widget.indeterm.IndeterminateCheckBox;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import butterknife.Unbinder;
 
 /**
@@ -24,6 +29,10 @@ public class CheckboxFragment extends Fragment {
     private static Component mInstance;
 
     Unbinder mUnbinder;
+    @BindView(R.id.cbEnable)
+    CheckBox cbEnable;
+    @BindView(R.id.cbEnableIndeterminate)
+    IndeterminateCheckBox cbEnableIndeterminate;
 
     public static Component getmInstance() {
         mInstance = new Component();
@@ -33,15 +42,31 @@ public class CheckboxFragment extends Fragment {
         return mInstance;
     }
 
-
     public CheckboxFragment() {
         // Required empty public constructor
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_checkbox, container, false);
+        View view = inflater.inflate(R.layout.fragment_checkbox, container, false);
+        mUnbinder = ButterKnife.bind(this, view);
+
+        cbEnable.setOnClickListener(view1 -> {
+            String status = cbEnable.isChecked() ? "Activo." : "Inactivo.";
+            Toast.makeText(getActivity(), status, Toast.LENGTH_SHORT).show();
+
+            cbEnableIndeterminate.setIndeterminate(cbEnable.isChecked());
+        });
+
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mUnbinder.unbind();
     }
 }
